c51e8a65810f7734a946710761dad697c20f26b2
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-04 18:36:07 +0100

    chore: Update ESLint configuration to include ESLint-config-prettier and TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,65 @@
-import { fixupConfigRules } from "@eslint/compat";
-import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from '@eslint/compat'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintConfigPrettier from 'eslint-config-prettier' // Importing eslint-config-prettier
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+    allConfig: js.configs.all,
+})
 
-export default [{
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
-}, ...fixupConfigRules(compat.extends(
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-)), {
-    plugins: {
-        "react-refresh": reactRefresh,
-        prettier,
+export default [
+    {
+        ignores: ['**/dist', '**/.eslintrc.cjs'],
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+    ...fixupConfigRules(
+        compat.extends(
+            'airbnb',
+            'airbnb-typescript',
+            'airbnb/hooks',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:react-hooks/recommended'
+        )
+    ),
+    {
+        plugins: {
+            'react-refresh': reactRefresh,
+            prettier,
         },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
 
-        parserOptions: {
-            project: "./tsconfig.app.json",
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+
+            parserOptions: {
+                project: './tsconfig.app.json',
+            },
+        },
+
+        rules: {
+            'react-refresh/only-export-components': [
+                'warn',
+                {
+                    allowConstantExport: true,
+                },
+            ],
+
+            'react/react-in-jsx-scope': 0,
         },
     },
-
-    rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
-
-        "react/react-in-jsx-scope": 0,
-    },
-}];
+    eslintConfigPrettier,
+] // Adding eslint-config-prettier to the configuration array
