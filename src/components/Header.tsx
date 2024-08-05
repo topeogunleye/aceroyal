3142702c7b13027638a9d9b2ae63cfa637978598
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-05 23:44:50 +0100

    feat: Add Header component for displaying logo
    The Header component is added to display the logo of ACE ROYAL FOODS in the header section of the application. This component will enhance the visual appeal and branding of the application.

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => (
+  <header>
+    <img
+      src="./images/logo-removebg.png"
+      alt="ACE ROYAL FOODS Logo"
+      style={{ height: '187px' }}
+    />
+  </header>
+);
+
+export default Header;
