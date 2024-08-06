fb15b16f30f6dd1d3b76608db22eb977b13ce6c7
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-06 08:03:45 +0100

    refactor: Update image source handling in Slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -8,7 +8,7 @@ interface SliderProps {
 const Slider: React.FC<SliderProps> = ({ imageSrc, isActive }) => (
   <div className={`slide-box ${isActive ? 'active' : ''}`}>
     <div className="slide-text">
-      <img src={`images/${imageSrc}`} alt={imageSrc.split('.')[0]} />
+      <img src={imageSrc} alt={imageSrc.split('/').pop()?.split('.')[0]} />
     </div>
   </div>
 );
