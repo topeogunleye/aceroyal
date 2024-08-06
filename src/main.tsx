de5cd7151695bc68e0d26c71344fe837f1fe0ae0
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-06 11:15:14 +0100

    refactor: Update CSS import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WrappedApp } from './App';
-import './index.css';
+import './assets/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
