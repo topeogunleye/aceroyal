cb5e069391b91b517040a2b9bb1ac26621b72b50
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-08 19:48:41 +0100

    refactor: Update Blog component layout and styling
    This commit improves the visual consistency and layout of the Blog component. It updates the CSS styles for the blog section, blog card, and its child elements. The changes include adding a span element to the blog title for styling purposes, adjusting the font color of the blog card's paragraph, and updating the background image, padding, and box shadow of the featured blog card. These updates enhance the overall appearance of the blog section and make it more appealing to users.

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,21 +1,45 @@
-import './Blog.css'
+import './Blog.css';
 
 const Blog: React.FC = () => {
   return (
     <section className="blog-section">
-      <h2>Our Blog</h2>
+      <h2>
+        Our <span>Blog</span>
+      </h2>
       <p>
         At vero eos et accusamus et iusto odio dignissimos ducimus qui
         blanditiis praesentium voluptatum deleniti atque...
       </p>
-      <div className="blog-card">
+      <div className="blog-card featured">
         <p>April 2, 2024</p>
         <h3>The Palate Pioneer: Navigating the World of Tastes</h3>
         <p>
           At vero eos et accusamus et iusto odio dignissimos ducimus qui
           blanditiis praesentium voluptatum deleniti atque...
         </p>
-        <a href="#">Read More</a>
+        <div className="read-more">Read More →</div>
+      </div>
+
+      <div className="bloglist">
+        <div className="blog-card">
+          <p>April 2, 2024</p>
+          <h3>The Culinary Canvas: Artistry on a Plate Buffet</h3>
+          <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque...
+          </p>
+          <div className="read-more">Read More →</div>
+        </div>
+
+        <div className="blog-card">
+          <p>April 2, 2024</p>
+          <h3>Tasting Traditions: A Culinary and Odyssey</h3>
+          <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque...
+          </p>
+          <div className="read-more">Read More →</div>
+        </div>
       </div>
     </section>
   );
